0df2980fd9362fcb65b085e70a97ed1990158d52
ariflabs <arifaalfatih@gmail.com>
2024-10-15 05:43:14 +0700

    added sidebar cart

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,56 @@
         <span>Past Orders</span>
       </router-link>
     </nav>
-    <!-- <router-link @click="toggleSidebar" class="top-bar-cart-link" :to=>
+    <div @click="toggleSidebar" class="top-bar-cart-link">
       <i class="icofont-cart-alt icofont-1x"></i>
       <span>Cart ({{ totalItemCart }})</span>
-    </router-link> -->
+    </div>
   </header>
-  <router-view/>
+  <router-view :inventory="inventory" />
+
+  <SidebarContent
+    v-if="showSidebar"
+    :toggle="toggleSidebar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
 </template>
 
-<style scoped>
+<script>
+import SidebarContent from '@/components/SidebarContent.vue'
+import food from './food.json'
 
-</style>
+export default {
+  components: {
+    SidebarContent
+  },
+  data () {
+    return {
+      showSidebar: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalItemCart () {
+      return Object.values(this.cart).reduce((acc, curr) => {
+        return acc + curr
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, index) {
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += this.inventory[index].quantity
+      this.inventory[index].quantity = 0
+    },
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -57,16 +57,9 @@
 </template>
 
 <script>
-import food from '../food.json'
 
 export default {
   name: 'HomeView',
-  data () {
-    return {
-      inventory: food
-    }
-  },
-  components: {
-  }
+  props: ['inventory']
 }
 </script>
